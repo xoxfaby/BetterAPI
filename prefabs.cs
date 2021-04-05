1dd6ac4e4578e4a31c0f52da3e39af5d73c3063b
EvaisaGiac <evagiacosa1@gmail.com>
2021-04-05 12:39:03 +0200

    Add interactablesAPI

---
+++ prefabs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RoR2;
+using UnityEngine;
+
+namespace BetterAPI
+{
+    public class Prefabs
+    {
+        internal static List<GameObject> prefabs;
+        static Prefabs()
+        {
+            prefabs = new List<GameObject>();
+        }
+        public static void Add(GameObject prefab)
+        {
+            prefabs.Add(prefab);
+        }
+
+    }
+}
