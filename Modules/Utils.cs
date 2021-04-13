8694c22dd9a6a3f2a1a02fcc2195177244b99117
XoXFaby <xoxfaby@gmail.com>
2021-04-13 23:11:52 +0200

    more commits

--- Modules/Utils.cs
+++ Modules/Utils.cs
@@ -50,16 +50,12 @@ namespace BetterAPI
             if (networkId)
             {
                 BetterAPI.print("Prefab already has NetworkID");
-                BetterAPI.print($"Original ID: {networkId.assetId.ToString()}");
-                var newnetworkId = NetworkHash128.Parse(networkId.assetId.ToString());
-                BetterAPI.print($"New ID: {newnetworkId.ToString()}");
                 if (!prefabCounter.ContainsKey(gameObject)) prefabCounter.Add(gameObject, 0);
                 prefabCounter[gameObject]++;
-                for (int i = 1; i < prefabCounter[gameObject]; i++)
-                {
-                    newnetworkId = NetworkHash128.Parse(newnetworkId.ToString());
-                    BetterAPI.print($"Rerolled ID, new ID: {newnetworkId.ToString()}");
-                }
+                BetterAPI.print($"Original ID: {networkId.assetId.ToString()}");
+                BetterAPI.print($"Copy #:{prefabCounter[gameObject]}");
+                var newnetworkId = NetworkHash128.Parse(networkId.assetId.ToString() + prefabCounter[gameObject]);
+                BetterAPI.print($"New ID: {newnetworkId.ToString()}");
                 ClientScene.RegisterPrefab(prefab, newnetworkId);
             }
             return prefab;
