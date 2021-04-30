ffefbf3560c12e9a7532e1a1e223d38d3ee65995
xoxfaby <xoxfaby@gmail.com>
2021-04-30 08:28:30 +0200

    Add Buff Names and Descriptions

--- Modules/Buffs.cs
+++ Modules/Buffs.cs
@@ -9,6 +9,7 @@ namespace BetterAPI
 {
     public static class Buffs
     {
+        internal static Dictionary<BuffDef, BuffInfo> buffInfos = new Dictionary<BuffDef, BuffInfo>();
         public static void Add(BuffDef buffDef)
         {
             Add(buffDef, Assembly.GetCallingAssembly().GetName().Name);
@@ -19,5 +20,42 @@ namespace BetterAPI
             ContentPacks.Packs[contentPackIdentifier].buffDefs.Add(buffDef);
         }
 
+        public static void AddName(BuffDef buffDef, string nameToken)
+        {
+            AddInfo(buffDef, nameToken: nameToken);
+        }
+        public static void AddDescription(BuffDef buffDef, string descriptionToken)
+        {
+            AddInfo(buffDef, descriptionToken: descriptionToken);
+        }
+
+        public static void AddInfo(BuffDef buffDef, string nameToken = null, string descriptionToken = null)
+        {
+            buffInfos.TryGetValue(buffDef, out BuffInfo buffInfo);
+            buffInfo.nameToken = buffInfo.nameToken ?? nameToken;
+            buffInfo.descriptionToken = buffInfo.descriptionToken ?? descriptionToken;
+            AddInfo(buffDef, buffInfo);
+        }
+        public static void AddInfo(BuffDef buffDef, BuffInfo buffInfo)
+        {
+            buffInfos[buffDef] = buffInfo;
+        }
+
+        public static string GetName(BuffDef buffDef)
+        {
+            buffInfos.TryGetValue(buffDef, out BuffInfo buffInfo);
+            return string.IsNullOrEmpty(buffInfo.nameToken) ? null : RoR2.Language.GetString(buffInfo.nameToken);
+        }
+        public static string GetDescription(BuffDef buffDef)
+        {
+            buffInfos.TryGetValue(buffDef, out BuffInfo buffInfo);
+            return string.IsNullOrEmpty(buffInfo.descriptionToken) ? null : RoR2.Language.GetString(buffInfo.descriptionToken);
+        }
+
+        public struct BuffInfo
+        {
+            public string nameToken;
+            public string descriptionToken;
+        }
     }
 }
