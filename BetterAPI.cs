89bf6053b1171fa38e7f59ee345305baca6d3ead
xoxfaby <xoxfaby@gmail.com>
2022-02-24 15:33:33 +0100

    Fix version

--- BetterAPI.cs
+++ BetterAPI.cs
@@ -3,12 +3,10 @@ using BepInEx;
 
 namespace BetterAPI
 {
-    [BepInPlugin("com.xoxfaby.BetterAPI", "BetterAPI", BetterAPIPlugin.Version + "+" + ReleaseType + "-" + BuildNumber)]
+    [BepInPlugin("com.xoxfaby.BetterAPI", "BetterAPI", BetterAPIPlugin.Version)]
     public class BetterAPIPlugin : BetterUnityPlugin.BetterUnityPlugin<BetterAPIPlugin>
     {
         public const string Version = "2.0.0";
-        public const string ReleaseType = "release";
-        public const string BuildNumber = "1";
         public override BaseUnityPlugin typeReference => throw new NotImplementedException();
     }
 }
