f61a358fb08516607aa0dc967a7f9bbe29d3a7be
Evaisa <evagiacosa1@gmail.com>
2022-03-03 16:30:22 +0100

    Update version string to "4.0.0"

--- BetterAPI.cs
+++ BetterAPI.cs
@@ -6,7 +6,7 @@ namespace BetterAPI
     [BepInPlugin("com.xoxfaby.BetterAPI", "BetterAPI", BetterAPIPlugin.Version)]
     public class BetterAPIPlugin : BetterUnityPlugin.BetterUnityPlugin<BetterAPIPlugin>
     {
-        public const string Version = "3.0.0";
+        public const string Version = "4.0.0";
         public override BaseUnityPlugin typeReference => throw new NotImplementedException();
 
         protected override void Awake()
